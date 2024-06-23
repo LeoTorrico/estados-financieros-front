2534b245b047923765099ee6a9f6b1e7098e533b
Leo Torrico <leonardotorrico50@gmail.com>
2024-06-23 19:40:33 -0400

    Arreglo estilos navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../Styles/Navbar.css'; // Actualiza la ruta del archivo CSS
-
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary my-app-custom-navbar">
         <div className="container-fluid">
           <a className="navbar-brand custom-brand" href="#">ACEFTOOL</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Inicio</a>
