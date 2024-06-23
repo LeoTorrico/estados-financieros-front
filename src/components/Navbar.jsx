4bfe4ae69456d426456441d56af1225baf1d2219
Leo Torrico <leonardotorrico50@gmail.com>
2024-06-23 18:46:15 -0400

    Implementacion estilos navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-
+import React from 'react';
+import '../Styles/Navbar.css'; // Actualiza la ruta del archivo CSS
 
 function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary custom-navbar">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">ACEFTOOL</a>
+          <a className="navbar-brand custom-brand" href="#">ACEFTOOL</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Inicio</a>
@@ -39,7 +39,7 @@ function Navbar() {
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
