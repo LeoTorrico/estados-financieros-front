6dcb415f423fcbeb5d66061e417d18c2744c896f
Leo Torrico <leonardotorrico50@gmail.com>
2024-06-27 21:25:37 -0400

    Asignacion imagen empresa

--- src/components/GetEmpresas.jsx
+++ src/components/GetEmpresas.jsx
@@ -3,6 +3,10 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import '../Styles/UploadExcel.css';
 
+import imgCard1 from '/src/assets/card1.png';
+import imgCard2 from '/src/assets/card2.webp';
+import imgCard3 from '/src/assets/card3.jpg';
+
 function GetEmpresas() {
   const [empresas, setEmpresas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -31,6 +35,12 @@ function GetEmpresas() {
           <div className="upload-excel-card-inner">
             <h3 className="card-title">{empresa.nombre_empresa}</h3>
             <h4>{empresa.nombre}</h4>
+            {/* Lógica para asignar imagen */}
+            <div className="card-image-container">
+              {empresa.nro_imagen === 1 && <img src={imgCard1} alt="Card 1" />}
+              {empresa.nro_imagen === 2 && <img src={imgCard2} alt="Card 2" />}
+              {empresa.nro_imagen === 3 && <img src={imgCard3} alt="Card 3" />}
+            </div>
             <button
               className="view-button"
               onClick={() => navigate(`/empresa/${empresa.cod_empresa}`)}
